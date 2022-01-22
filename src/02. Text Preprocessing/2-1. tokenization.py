06f8beb10cc46d326c654da0f39c51e2f4e44bac
mecha2k <mecha2k@naver.com>
2022-01-22 13:08:22 +0900

    text preprocessing

--- src/02. Text Preprocessing/2-1. tokenization.py
+++ src/02. Text Preprocessing/2-1. tokenization.py
@@ -56,7 +56,11 @@ if __name__ == "__main__":
     print("트리뱅크 워드토크나이저 :", tokenizer.tokenize(text))
 
     # 4. 문장 토큰화(Sentence Tokenization)
-    text = "His barber kept his word. But keeping such a huge secret to himself was driving him crazy. Finally, the barber went up a mountain and almost to the edge of a cliff. He dug a hole in the midst of some reeds. He looked about, to make sure no one was near."
+    text = (
+        "His barber kept his word. But keeping such a huge secret to himself was driving him crazy. Finally, "
+        "the barber went up a mountain and almost to the edge of a cliff. He dug a hole in the midst of some reeds. "
+        "He looked about, to make sure no one was near."
+    )
     print("문장 토큰화1 :", sent_tokenize(text))
 
     text = "I am actively looking for Ph.D. students. and you are a Ph.D student."
