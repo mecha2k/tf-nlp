4b5a45d870d2225953a5af8638981e9a6f016f20
mecha2k <mecha2k@naver.com>
2022-01-26 11:11:05 +0900

    rnn classification

--- src/10. RNN Text Classification/10-2. spam_detection.py
+++ src/10. RNN Text Classification/10-2. spam_detection.py
@@ -1,7 +1,4 @@
 import tensorflow as tf
-
-# 1. 스팸 메일 데이터에 대한 이해
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,24 +6,18 @@ import urllib.request
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import SimpleRNN, Embedding, Dense
+from tensorflow.keras.models import Sequential
 
-# urllib.request.urlretrieve(
-#     "https://raw.githubusercontent.com/ukairia777/tensorflow-nlp-tutorial/main/10.%20RNN%20Text%20Classification/dataset/spam.csv",
-#     filename="../data/spam.csv",
-# )
 data = pd.read_csv("../data/spam.csv", encoding="latin1")
-
 print("총 샘플의 수 :", len(data))
-print(data[:5])
-
-del data["Unnamed: 2"]
-del data["Unnamed: 3"]
-del data["Unnamed: 4"]
-data["v1"] = data["v1"].replace(["ham", "spam"], [0, 1])
-print(data[:5])
 data.info()
 
-print("결측값 여부 :", data.isnull().values.any())
+data = data[["v1", "v2"]]
+data["v1"] = data["v1"].replace(["ham", "spam"], [0, 1])
+data.info()
+
+print("결측값 여부 :", data.isna().any())
 print("v2열의 유니크한 값 :", data["v2"].nunique())
 
 # v2 열에서 중복인 내용이 있다면 중복 제거
@@ -34,10 +25,10 @@ data.drop_duplicates(subset=["v2"], inplace=True)
 print("총 샘플의 수 :", len(data))
 
 data["v1"].value_counts().plot(kind="bar")
+plt.savefig("images/02-01", dpi=300)
 
 print("정상 메일과 스팸 메일의 개수")
 print(data.groupby("v1").size().reset_index(name="count"))
-
 print(f'정상 메일의 비율 = {round(data["v1"].value_counts()[0]/len(data) * 100,3)}%')
 print(f'스팸 메일의 비율 = {round(data["v1"].value_counts()[1]/len(data) * 100,3)}%')
 
@@ -65,7 +56,6 @@ print(f"스팸 메일 = {round(y_test.value_counts()[1]/len(y_test) * 100,3)}%")
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(X_train)
 X_train_encoded = tokenizer.texts_to_sequences(X_train)
-
 print(X_train_encoded[:5])
 
 word_to_index = tokenizer.word_index
@@ -95,31 +85,27 @@ print("단어 집합의 크기: {}".format(vocab_size))
 
 print("메일의 최대 길이 : %d" % max(len(l) for l in X_train_encoded))
 print("메일의 평균 길이 : %f" % (sum(map(len, X_train_encoded)) / len(X_train_encoded)))
+
+fig = plt.figure(figsize=(10, 6))
 plt.hist([len(s) for s in X_data], bins=50)
 plt.xlabel("length of samples")
 plt.ylabel("number of samples")
-plt.show()
+plt.savefig("images/02-02", dpi=300)
 
-max_len = 189
+max_len = 200
 X_train_padded = pad_sequences(X_train_encoded, maxlen=max_len)
 print("훈련 데이터의 크기(shape): ", X_train_padded.shape)
 
-# 2. RNN으로 스팸 메일 분류하기
-
-from tensorflow.keras.layers import SimpleRNN, Embedding, Dense
-from tensorflow.keras.models import Sequential
 
 model = Sequential()
 model.add(Embedding(vocab_size, 32))  # 임베딩 벡터의 차원은 32
 model.add(SimpleRNN(32))  # RNN 셀의 hidden_size는 32
 model.add(Dense(1, activation="sigmoid"))
-
 model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["acc"])
 history = model.fit(X_train_padded, y_train, epochs=4, batch_size=64, validation_split=0.2)
 
 X_test_encoded = tokenizer.texts_to_sequences(X_test)
 X_test_padded = pad_sequences(X_test_encoded, maxlen=max_len)
-
 print("\n 테스트 정확도: %.4f" % (model.evaluate(X_test_padded, y_test)[1]))
 
 epochs = range(1, len(history.history["acc"]) + 1)
@@ -129,4 +115,4 @@ plt.title("model loss")
 plt.ylabel("loss")
 plt.xlabel("epoch")
 plt.legend(["train", "val"], loc="upper left")
-plt.show()
+plt.savefig("images/02-03", dpi=300)
--- src/10. RNN Text Classification/10-3. reuters_news_classification.py
+++ src/10. RNN Text Classification/10-3. reuters_news_classification.py
@@ -1,7 +1,4 @@
 import tensorflow as tf
-
-# 1. 로이터 뉴스 데이터에 대한 이해
-
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -11,6 +8,7 @@ from tensorflow.keras.datasets import reuters
 
 print("훈련용 뉴스 기사 : {}".format(len(X_train)))
 print("테스트용 뉴스 기사 : {}".format(len(X_test)))
+
 num_classes = len(set(y_train))
 print("카테고리 : {}".format(num_classes))
 
@@ -20,36 +18,34 @@ print(y_train[0])  # 첫번째 훈련용 뉴스 기사의 레이블
 print("뉴스 기사의 최대 길이 :{}".format(max(len(l) for l in X_train)))
 print("뉴스 기사의 평균 길이 :{}".format(sum(map(len, X_train)) / len(X_train)))
 
-plt.hist([len(s) for s in X_train], bins=50)
+plt.hist(x=[len(s) for s in X_train], bins=50)
 plt.xlabel("length of samples")
 plt.ylabel("number of samples")
-plt.show()
+plt.savefig("images/03-01", dpi=300)
 
 fig, axe = plt.subplots(ncols=1)
 fig.set_size_inches(12, 5)
-sns.countplot(y_train)
+sns.countplot(data=y_train)
+plt.savefig("images/03-02", dpi=300)
 
 unique_elements, counts_elements = np.unique(y_train, return_counts=True)
 print("각 레이블에 대한 빈도수:")
 print(np.asarray((unique_elements, counts_elements)))
 
 word_to_index = reuters.get_word_index()
-print(word_to_index)
+# print(word_to_index)
 
 index_to_word = {}
 for key, value in word_to_index.items():
     index_to_word[value + 3] = key
 
 print("빈도수 상위 1번 단어 : {}".format(index_to_word[4]))
-
 print("빈도수 상위 128등 단어 : {}".format(index_to_word[131]))
 
 for index, token in enumerate(("<pad>", "<sos>", "<unk>")):
     index_to_word[index] = token
-
 print(" ".join([index_to_word[index] for index in X_train[0]]))
 
-# 2. LSTM으로 로이터 뉴스 분류하기
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Embedding
@@ -63,7 +59,6 @@ from tensorflow.keras.models import load_model
 max_len = 100
 X_train = pad_sequences(X_train, maxlen=max_len)  # 훈련용 뉴스 기사 패딩
 X_test = pad_sequences(X_test, maxlen=max_len)  # 테스트용 뉴스 기사 패딩
-
 y_train = to_categorical(y_train)  # 훈련용 뉴스 기사 레이블의 원-핫 인코딩
 y_test = to_categorical(y_test)  # 테스트용 뉴스 기사 레이블의 원-핫 인코딩
 
@@ -76,13 +71,12 @@ model = Sequential()
 model.add(Embedding(vocab_size, embedding_dim))
 model.add(LSTM(hidden_units))
 model.add(Dense(num_classes, activation="softmax"))
-
 es = EarlyStopping(monitor="val_loss", mode="min", verbose=1, patience=4)
 mc = ModelCheckpoint(
     "../data/best_model.h5", monitor="val_acc", mode="max", verbose=1, save_best_only=True
 )
-
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["acc"])
+model.summary()
 
 history = model.fit(
     X_train,
@@ -97,10 +91,11 @@ loaded_model = load_model("../data/best_model.h5")
 print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
 
 epochs = range(1, len(history.history["acc"]) + 1)
+fig = plt.figure(figsize=(10, 6))
 plt.plot(epochs, history.history["loss"])
 plt.plot(epochs, history.history["val_loss"])
 plt.title("model loss")
 plt.ylabel("loss")
 plt.xlabel("epoch")
 plt.legend(["train", "test"], loc="upper left")
-plt.show()
+plt.savefig("images/03-03", dpi=300)
