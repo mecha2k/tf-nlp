ff8005a086fc7e608abe0f85d54530648f712392
mecha2k <mecha2k@naver.com>
2022-01-28 11:07:35 +0900

    transformer

--- src/16. Transformer/16-1 ~ 16-2. transformer_chatbot.py
+++ src/16. Transformer/16-1 ~ 16-2. transformer_chatbot.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import urllib.request
 import tensorflow_datasets as tfds
 import time, re
+from icecream import ic
 
 
 class PositionalEncoding(tf.keras.layers.Layer):
@@ -347,16 +348,44 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
         return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
 
+def pos_encoding_attention_ex():
+    sample_pos_encoding = PositionalEncoding(50, 128)
+
+    plt.pcolormesh(sample_pos_encoding.pos_encoding.numpy()[0], cmap="RdBu")
+    plt.xlabel("Depth")
+    plt.xlim((0, 128))
+    plt.ylabel("Position")
+    plt.colorbar()
+    plt.savefig("images/01-01", dpi=300)
+
+    # 임의의 Query, Key, Value인 Q, K, V 행렬 생성
+    np.set_printoptions(precision=3, suppress=True)
+
+    temp_k = tf.constant([[10, 0, 0], [0, 10, 0], [0, 0, 10], [0, 0, 10]], dtype=tf.float32)
+    temp_v = tf.constant([[1, 0], [10, 0], [100, 5], [1000, 6]], dtype=tf.float32)
+    temp_q = tf.constant([[0, 10, 0]], dtype=tf.float32)
+    ic(temp_q)
+    ic(temp_k)
+    ic(temp_v)
+
+    temp_out, temp_attn = scaled_dot_product_attention(temp_q, temp_k, temp_v, None)
+    ic(temp_attn)  # 어텐션 분포(어텐션 가중치의 나열)
+    ic(temp_out)  # 어텐션 값
+
+    temp_q = tf.constant([[0, 0, 10]], dtype=tf.float32)
+    temp_out, temp_attn = scaled_dot_product_attention(temp_q, temp_k, temp_v, None)
+    ic(temp_attn)  # 어텐션 분포(어텐션 가중치의 나열)
+    ic(temp_out)  # 어텐션 값
+
+    temp_q = tf.constant([[0, 0, 10], [0, 10, 0], [10, 10, 0]], dtype=tf.float32)  # (3, 3)
+    temp_out, temp_attn = scaled_dot_product_attention(temp_q, temp_k, temp_v, None)
+    ic(temp_attn)  # 어텐션 분포(어텐션 가중치의 나열)
+    ic(temp_out)  # 어텐션 값
+
+
 if __name__ == "__main__":
-    # sample_pos_encoding = PositionalEncoding(50, 128)
-    #
-    # plt.pcolormesh(sample_pos_encoding.pos_encoding.numpy()[0], cmap="RdBu")
-    # plt.xlabel("Depth")
-    # plt.xlim((0, 128))
-    # plt.ylabel("Position")
-    # plt.colorbar()
-    # plt.savefig("images/01-01", dpi=300)
-    #
+    pos_encoding_attention_ex()
+
     # small_transformer = transformer(
     #     vocab_size=9000,
     #     num_layers=4,
@@ -377,202 +406,202 @@ if __name__ == "__main__":
     # plt.xlabel("Train Step")
     # plt.savefig("images/01-02", dpi=300)
 
-    train_data = pd.read_csv("../data/ChatBotData.csv")
-    print(train_data.head())
-    print("챗봇 샘플의 개수 :", len(train_data))
-    print(train_data.isna().sum())
-
-    questions = []
-    for sentence in train_data["Q"]:
-        # 구두점에 대해서 띄어쓰기
-        # ex) 12시 땡! -> 12시 땡 !
-        sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-        sentence = sentence.strip()
-        questions.append(sentence)
-
-    answers = []
-    for sentence in train_data["A"]:
-        sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-        sentence = sentence.strip()
-        answers.append(sentence)
-
-    print(len(questions))
-    print(questions[:5])
-    print(answers[:5])
-
-    # 서브워드텍스트인코더를 사용하여 질문과 답변을 모두 포함한 단어 집합(Vocabulary) 생성
-    tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
-        questions + answers, target_vocab_size=2 ** 13
-    )
-
-    # 시작 토큰과 종료 토큰에 대한 정수 부여.
-    START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
-
-    # 시작 토큰과 종료 토큰을 고려하여 단어 집합의 크기를 + 2
-    VOCAB_SIZE = tokenizer.vocab_size + 2
-
-    print("시작 토큰 번호 :", START_TOKEN)
-    print("종료 토큰 번호 :", END_TOKEN)
-    print("단어 집합의 크기 :", VOCAB_SIZE)
-
-    # 서브워드텍스트인코더 토크나이저의 .encode()를 사용하여 텍스트 시퀀스를 정수 시퀀스로 변환.
-    print("Tokenized sample question: {}".format(tokenizer.encode(questions[20])))
-
-    # 서브워드텍스트인코더 토크나이저의 .encode()와 decode() 테스트해보기
-    # 임의의 입력 문장을 sample_string에 저장
-    sample_string = questions[20]
-
-    # encode() : 텍스트 시퀀스 --> 정수 시퀀스
-    tokenized_string = tokenizer.encode(sample_string)
-    print("정수 인코딩 후의 문장 {}".format(tokenized_string))
-
-    # decode() : 정수 시퀀스 --> 텍스트 시퀀스
-    original_string = tokenizer.decode(tokenized_string)
-    print("기존 문장: {}".format(original_string))
-
-    # 각 정수는 각 단어와 어떻게 mapping되는지 병렬로 출력
-    # 서브워드텍스트인코더는 의미있는 단위의 서브워드로 토크나이징한다. 띄어쓰기 단위 X 형태소 분석 단위 X
-    for ts in tokenized_string:
-        print("{} ----> {}".format(ts, tokenizer.decode([ts])))
-
-    # 최대 길이를 40으로 정의
-    MAX_LENGTH = 40
-
-    # 토큰화 / 정수 인코딩 / 시작 토큰과 종료 토큰 추가 / 패딩
-    def tokenize_and_filter(inputs, outputs):
-        tokenized_inputs, tokenized_outputs = [], []
-
-        for (sentence1, sentence2) in zip(inputs, outputs):
-            # encode(토큰화 + 정수 인코딩), 시작 토큰과 종료 토큰 추가
-            sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
-            sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
-
-            tokenized_inputs.append(sentence1)
-            tokenized_outputs.append(sentence2)
-
-        # 패딩
-        tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
-            tokenized_inputs, maxlen=MAX_LENGTH, padding="post"
-        )
-        tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
-            tokenized_outputs, maxlen=MAX_LENGTH, padding="post"
-        )
-
-        return tokenized_inputs, tokenized_outputs
-
-    questions, answers = tokenize_and_filter(questions, answers)
-
-    print("질문 데이터의 크기(shape) :", questions.shape)
-    print("답변 데이터의 크기(shape) :", answers.shape)
-
-    # 0번째 샘플을 임의로 출력
-    print(questions[0])
-    print(answers[0])
-
-    print("단어 집합의 크기(Vocab size): {}".format(VOCAB_SIZE))
-    print("전체 샘플의 수(Number of samples): {}".format(len(questions)))
-
-    # 텐서플로우 dataset을 이용하여 셔플(shuffle)을 수행하되, 배치 크기로 데이터를 묶는다.
-    # 또한 이 과정에서 교사 강요(teacher forcing)을 사용하기 위해서 디코더의 입력과 실제값 시퀀스를 구성한다.
-    BATCH_SIZE = 64
-    BUFFER_SIZE = 20000
-
-    # 디코더의 실제값 시퀀스에서는 시작 토큰을 제거해야 한다.
-    dataset = tf.data.Dataset.from_tensor_slices(
-        (
-            {"inputs": questions, "dec_inputs": answers[:, :-1]},  # 디코더의 입력. 마지막 패딩 토큰이 제거된다.
-            {"outputs": answers[:, 1:]},  # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다.
-        )
-    )
-
-    dataset = dataset.cache()
-    dataset = dataset.shuffle(BUFFER_SIZE)
-    dataset = dataset.batch(BATCH_SIZE)
-    dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
-
-    # 임의의 샘플에 대해서 [:, :-1]과 [:, 1:]이 어떤 의미를 가지는지 테스트해본다.
-    print(answers[0])  # 기존 샘플
-    print(answers[:1][:, :-1])  # 마지막 패딩 토큰 제거하면서 길이가 39가 된다.
-    print(answers[:1][:, 1:])  # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다. 길이는 역시 39가 된다.
-
-    tf.keras.backend.clear_session()
-
-    # Hyper-parameters
-    NUM_LAYERS = 2
-    D_MODEL = 256
-    NUM_HEADS = 8
-    DFF = 512
-    DROPOUT = 0.1
-
-    model = transformer(
-        vocab_size=VOCAB_SIZE,
-        num_layers=NUM_LAYERS,
-        dff=DFF,
-        d_model=D_MODEL,
-        num_heads=NUM_HEADS,
-        dropout=DROPOUT,
-    )
-
-    learning_rate = CustomSchedule(D_MODEL)
-    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
-
-    def accuracy(y_true, y_pred):
-        # ensure labels have shape (batch_size, MAX_LENGTH - 1)
-        y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
-        return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
-
-    def loss_function(y_true, y_pred):
-        y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
-
-        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="none")(
-            y_true, y_pred
-        )
-
-        mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
-        loss = tf.multiply(loss, mask)
-
-        return tf.reduce_mean(loss)
-
-    model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
-
-    EPOCHS = 2
-    model.fit(dataset, epochs=EPOCHS)
-
-    def evaluate(sentence):
-        sentence = preprocess_sentence(sentence)
-        sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
-        output = tf.expand_dims(START_TOKEN, 0)
-
-        # 디코더의 예측 시작
-        for i in range(MAX_LENGTH):
-            predictions = model(inputs=[sentence, output], training=False)
-            # 현재(마지막) 시점의 예측 단어를 받아온다.
-            predictions = predictions[:, -1:, :]
-            predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
-            # 만약 마지막 시점의 예측 단어가 종료 토큰이라면 예측을 중단
-            if tf.equal(predicted_id, END_TOKEN[0]):
-                break
-            # 마지막 시점의 예측 단어를 출력에 연결한다.
-            # 이는 for문을 통해서 디코더의 입력으로 사용될 예정이다.
-            output = tf.concat([output, predicted_id], axis=-1)
-
-        return tf.squeeze(output, axis=0)
-
-    def predict(sentence):
-        prediction = evaluate(sentence)
-        predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
-        print("Input: {}".format(sentence))
-        print("Output: {}".format(predicted_sentence))
-        return predicted_sentence
-
-    def preprocess_sentence(sentence):
-        sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
-        sentence = sentence.strip()
-        return sentence
-
-    print(predict("영화 볼래?"))
-    print(predict("고민이 있어"))
-    print(predict("너무 화가나"))
-    print(predict("게임하고싶은데 할래?"))
-    print(predict("나 너 좋아하는 것 같아"))
-    print(predict("딥 러닝 자연어 처리를 잘 하고 싶어"))
+    # train_data = pd.read_csv("../data/ChatBotData.csv")
+    # print(train_data.head())
+    # print("챗봇 샘플의 개수 :", len(train_data))
+    # print(train_data.isna().sum())
+    #
+    # questions = []
+    # for sentence in train_data["Q"]:
+    #     # 구두점에 대해서 띄어쓰기
+    #     # ex) 12시 땡! -> 12시 땡 !
+    #     sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    #     sentence = sentence.strip()
+    #     questions.append(sentence)
+    #
+    # answers = []
+    # for sentence in train_data["A"]:
+    #     sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    #     sentence = sentence.strip()
+    #     answers.append(sentence)
+    #
+    # print(len(questions))
+    # print(questions[:5])
+    # print(answers[:5])
+    #
+    # # 서브워드텍스트인코더를 사용하여 질문과 답변을 모두 포함한 단어 집합(Vocabulary) 생성
+    # tokenizer = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+    #     questions + answers, target_vocab_size=2 ** 13
+    # )
+    #
+    # # 시작 토큰과 종료 토큰에 대한 정수 부여.
+    # START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
+    #
+    # # 시작 토큰과 종료 토큰을 고려하여 단어 집합의 크기를 + 2
+    # VOCAB_SIZE = tokenizer.vocab_size + 2
+    #
+    # print("시작 토큰 번호 :", START_TOKEN)
+    # print("종료 토큰 번호 :", END_TOKEN)
+    # print("단어 집합의 크기 :", VOCAB_SIZE)
+    #
+    # # 서브워드텍스트인코더 토크나이저의 .encode()를 사용하여 텍스트 시퀀스를 정수 시퀀스로 변환.
+    # print("Tokenized sample question: {}".format(tokenizer.encode(questions[20])))
+    #
+    # # 서브워드텍스트인코더 토크나이저의 .encode()와 decode() 테스트해보기
+    # # 임의의 입력 문장을 sample_string에 저장
+    # sample_string = questions[20]
+    #
+    # # encode() : 텍스트 시퀀스 --> 정수 시퀀스
+    # tokenized_string = tokenizer.encode(sample_string)
+    # print("정수 인코딩 후의 문장 {}".format(tokenized_string))
+    #
+    # # decode() : 정수 시퀀스 --> 텍스트 시퀀스
+    # original_string = tokenizer.decode(tokenized_string)
+    # print("기존 문장: {}".format(original_string))
+    #
+    # # 각 정수는 각 단어와 어떻게 mapping되는지 병렬로 출력
+    # # 서브워드텍스트인코더는 의미있는 단위의 서브워드로 토크나이징한다. 띄어쓰기 단위 X 형태소 분석 단위 X
+    # for ts in tokenized_string:
+    #     print("{} ----> {}".format(ts, tokenizer.decode([ts])))
+    #
+    # # 최대 길이를 40으로 정의
+    # MAX_LENGTH = 40
+    #
+    # # 토큰화 / 정수 인코딩 / 시작 토큰과 종료 토큰 추가 / 패딩
+    # def tokenize_and_filter(inputs, outputs):
+    #     tokenized_inputs, tokenized_outputs = [], []
+    #
+    #     for (sentence1, sentence2) in zip(inputs, outputs):
+    #         # encode(토큰화 + 정수 인코딩), 시작 토큰과 종료 토큰 추가
+    #         sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    #         sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+    #
+    #         tokenized_inputs.append(sentence1)
+    #         tokenized_outputs.append(sentence2)
+    #
+    #     # 패딩
+    #     tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+    #         tokenized_inputs, maxlen=MAX_LENGTH, padding="post"
+    #     )
+    #     tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+    #         tokenized_outputs, maxlen=MAX_LENGTH, padding="post"
+    #     )
+    #
+    #     return tokenized_inputs, tokenized_outputs
+    #
+    # questions, answers = tokenize_and_filter(questions, answers)
+    #
+    # print("질문 데이터의 크기(shape) :", questions.shape)
+    # print("답변 데이터의 크기(shape) :", answers.shape)
+    #
+    # # 0번째 샘플을 임의로 출력
+    # print(questions[0])
+    # print(answers[0])
+    #
+    # print("단어 집합의 크기(Vocab size): {}".format(VOCAB_SIZE))
+    # print("전체 샘플의 수(Number of samples): {}".format(len(questions)))
+    #
+    # # 텐서플로우 dataset을 이용하여 셔플(shuffle)을 수행하되, 배치 크기로 데이터를 묶는다.
+    # # 또한 이 과정에서 교사 강요(teacher forcing)을 사용하기 위해서 디코더의 입력과 실제값 시퀀스를 구성한다.
+    # BATCH_SIZE = 64
+    # BUFFER_SIZE = 20000
+    #
+    # # 디코더의 실제값 시퀀스에서는 시작 토큰을 제거해야 한다.
+    # dataset = tf.data.Dataset.from_tensor_slices(
+    #     (
+    #         {"inputs": questions, "dec_inputs": answers[:, :-1]},  # 디코더의 입력. 마지막 패딩 토큰이 제거된다.
+    #         {"outputs": answers[:, 1:]},  # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다.
+    #     )
+    # )
+    #
+    # dataset = dataset.cache()
+    # dataset = dataset.shuffle(BUFFER_SIZE)
+    # dataset = dataset.batch(BATCH_SIZE)
+    # dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+    #
+    # # 임의의 샘플에 대해서 [:, :-1]과 [:, 1:]이 어떤 의미를 가지는지 테스트해본다.
+    # print(answers[0])  # 기존 샘플
+    # print(answers[:1][:, :-1])  # 마지막 패딩 토큰 제거하면서 길이가 39가 된다.
+    # print(answers[:1][:, 1:])  # 맨 처음 토큰이 제거된다. 다시 말해 시작 토큰이 제거된다. 길이는 역시 39가 된다.
+    #
+    # tf.keras.backend.clear_session()
+    #
+    # # Hyper-parameters
+    # NUM_LAYERS = 2
+    # D_MODEL = 256
+    # NUM_HEADS = 8
+    # DFF = 512
+    # DROPOUT = 0.1
+    #
+    # model = transformer(
+    #     vocab_size=VOCAB_SIZE,
+    #     num_layers=NUM_LAYERS,
+    #     dff=DFF,
+    #     d_model=D_MODEL,
+    #     num_heads=NUM_HEADS,
+    #     dropout=DROPOUT,
+    # )
+    #
+    # learning_rate = CustomSchedule(D_MODEL)
+    # optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+    #
+    # def accuracy(y_true, y_pred):
+    #     # ensure labels have shape (batch_size, MAX_LENGTH - 1)
+    #     y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+    #     return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+    #
+    # def loss_function(y_true, y_pred):
+    #     y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+    #
+    #     loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction="none")(
+    #         y_true, y_pred
+    #     )
+    #
+    #     mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    #     loss = tf.multiply(loss, mask)
+    #
+    #     return tf.reduce_mean(loss)
+    #
+    # model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+    #
+    # EPOCHS = 2
+    # model.fit(dataset, epochs=EPOCHS)
+    #
+    # def evaluate(sentence):
+    #     sentence = preprocess_sentence(sentence)
+    #     sentence = tf.expand_dims(START_TOKEN + tokenizer.encode(sentence) + END_TOKEN, axis=0)
+    #     output = tf.expand_dims(START_TOKEN, 0)
+    #
+    #     # 디코더의 예측 시작
+    #     for i in range(MAX_LENGTH):
+    #         predictions = model(inputs=[sentence, output], training=False)
+    #         # 현재(마지막) 시점의 예측 단어를 받아온다.
+    #         predictions = predictions[:, -1:, :]
+    #         predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+    #         # 만약 마지막 시점의 예측 단어가 종료 토큰이라면 예측을 중단
+    #         if tf.equal(predicted_id, END_TOKEN[0]):
+    #             break
+    #         # 마지막 시점의 예측 단어를 출력에 연결한다.
+    #         # 이는 for문을 통해서 디코더의 입력으로 사용될 예정이다.
+    #         output = tf.concat([output, predicted_id], axis=-1)
+    #
+    #     return tf.squeeze(output, axis=0)
+    #
+    # def predict(sentence):
+    #     prediction = evaluate(sentence)
+    #     predicted_sentence = tokenizer.decode([i for i in prediction if i < tokenizer.vocab_size])
+    #     print("Input: {}".format(sentence))
+    #     print("Output: {}".format(predicted_sentence))
+    #     return predicted_sentence
+    #
+    # def preprocess_sentence(sentence):
+    #     sentence = re.sub(r"([?.!,])", r" \1 ", sentence)
+    #     sentence = sentence.strip()
+    #     return sentence
+    #
+    # print(predict("영화 볼래?"))
+    # print(predict("고민이 있어"))
+    # print(predict("너무 화가나"))
+    # print(predict("게임하고싶은데 할래?"))
+    # print(predict("나 너 좋아하는 것 같아"))
+    # print(predict("딥 러닝 자연어 처리를 잘 하고 싶어"))
