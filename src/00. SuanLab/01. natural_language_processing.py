da709ca9a84318d72d1312ec59d20eaf00b2a2dc
mecha2k <mecha2k@naver.com>
2022-03-27 10:37:23 +0900

    nlp_suan

---
+++ src/00. SuanLab/01. natural_language_processing.py
@@ -0,0 +1,8 @@
+from konlpy.tag import Mecab
+
+mecab = Mecab(dicpath="c:/mecab/mecab-ko-dic")
+
+sent = "언제나 현재에 집중하면 행복할 것이다."
+print(mecab.pos(sent))
+print(mecab.morphs(sent))
+print(mecab.nouns(sent))
