b06742438e4e8f594d343402ee98318453f13cdb
mecha2k <mecha2k@naver.com>
2022-04-05 14:53:47 +0900

    suanlab-transformer

--- src/00. SuanLab/13. transformer_keras.py
+++ src/00. SuanLab/13. transformer_keras.py
@@ -4,8 +4,12 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 
-from tensorflow.keras.layers import Dense, Dropout, Embedding
-
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Embedding, Dense, Dropout, LayerNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.utils import plot_model
 
 np.random.seed(42)
 np.set_printoptions(precision=3, suppress=True)
@@ -15,6 +19,114 @@ plt.rcParams["figure.dpi"] = 200
 plt.rcParams["font.family"] = "NanumBarunGothic"
 plt.rcParams["axes.unicode_minus"] = False
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
+
+
+max_len = 25
+embed_dim = 128
+ffn_dim = 128
+num_heads = 8
+num_layers = 2
+
+epochs = 1
+batch_size = 256
+dropout = 0.2
+learning_rate = 0.001
+xavier_initializer = True
+
+
+from sklearn.model_selection import train_test_split
+from konlpy.tag import Okt
+import re
+
+okt = Okt()
+
+(PAD, STA, END, UNK) = ("<PAD>", "<STA>", "<END>", "<UNK>")
+(PAD_IDX, STA_IDX, END_IDX, UNK_IDX) = (0, 1, 2, 3)
+
+pattern = re.compile(pattern="([~.,!?\"':;()])")
+
+
+def apply_morphs(lines):
+    return [" ".join(okt.morphs(line.replace(" ", ""))) for line in lines]
+
+
+def encoder_preprocessing(lines, dictionary):
+    sentences, sent_len = [], []
+    for line in lines:
+        line = re.sub(pattern=pattern, repl="", string=line)
+        sentence = []
+        for word in line.split():
+            if dictionary.get(word) is not None:
+                sentence.extend([dictionary[word]])
+            else:
+                sentence.extend([dictionary[UNK]])
+        if len(sentence) > max_len:
+            sentence = sentence[:max_len]
+        sent_len.append(len(sentence))
+        sentence += (max_len - len(sentence)) * [dictionary[PAD]]
+        sentences.append(sentence)
+    return np.array(sentences), sent_len
+
+
+def decoder_output_preprocessing(lines, dictionary):
+    sentences, sent_len = [], []
+    for line in lines:
+        line = re.sub(pattern=pattern, repl="", string=line)
+        sentence = [dictionary[STA]] + [dictionary[word] for word in line.split()]
+        if len(sentence) > max_len:
+            sentence = sentence[:max_len]
+        sent_len.append(len(sentence))
+        sentence += (max_len - len(sentence)) * [dictionary[PAD]]
+        sentences.append(sentence)
+    return np.array(sentences), sent_len
+
+
+def decoder_target_preprocessing(lines, dictionary):
+    sentences = []
+    for line in lines:
+        line = re.sub(pattern=pattern, repl="", string=line)
+        sentence = [dictionary[word] for word in line.split()]
+        if len(sentence) >= max_len:
+            sentence = sentence[: max_len - 1] + [dictionary[END]]
+        else:
+            sentence += [dictionary[END]]
+        sentence += (max_len - len(sentence)) * [dictionary[PAD]]
+        sentences.append(sentence)
+    return np.array(sentences)
+
+
+cha2idx, idx2cha = np.load(file="../data/tf_dict.npy", allow_pickle=True)
+train_input, train_output, train_target = np.load(file="../data/tf_train.npy", allow_pickle=True)
+valid_input, valid_output, valid_target = np.load(file="../data/tf_valid.npy", allow_pickle=True)
+vocab_size = len(cha2idx)
+print(vocab_size)
+for line in valid_output[:5]:
+    text = [idx2cha[x] + " " for x in line if idx2cha[x] != PAD]
+    print("".join(text))
+for line in valid_target[:5]:
+    text = [idx2cha[x] + " " for x in line if idx2cha[x] != PAD]
+    print("".join(text))
+print(train_input.shape)
+print(train_output.shape)
+print(train_target.shape)
+print(valid_input.shape)
+print(valid_output.shape)
+print(valid_target.shape)
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_input, train_output, train_target))
+train_ds = (
+    train_ds.shuffle(10000)
+    .batch(batch_size)
+    .map(lambda x, y, z: ({"inputs": x, "outputs": y}, z), num_parallel_calls=4)
+    .repeat()
+)
+
+valid_ds = tf.data.Dataset.from_tensor_slices((valid_input, valid_output, valid_target))
+valid_ds = (
+    valid_ds.shuffle(10000)
+    .batch(batch_size)
+    .map(lambda x, y, z: ({"inputs": x, "outputs": y}, z), num_parallel_calls=4)
+)
 
 
 def positional_encoding(max_len, embed_dim):
@@ -80,71 +192,6 @@ outputs = multi_head_attention(query, key, value, embed_dim=64, num_heads=8)
 print(outputs.shape)
 
 
-from sklearn.model_selection import train_test_split
-from konlpy.tag import Okt
-import re
-
-okt = Okt()
-
-(PAD, STA, END, UNK) = ("<PAD>", "<STA>", "<END>", "<UNK>")
-(PAD_IDX, STA_IDX, END_IDX, UNK_IDX) = (0, 1, 2, 3)
-
-pattern = re.compile(pattern="([~.,!?\"':;()])")
-
-
-def apply_morphs(lines):
-    return [" ".join(okt.morphs(line.replace(" ", ""))) for line in lines]
-
-
-def index2string(lines, dictionary):
-    sentence = []
-    finished = False
-    for line in lines:
-        sentence = [dictionary[idx] for idx in line["indexs"]]
-    answer = ""
-    for word in sentence:
-        if word == END:
-            finished = True
-            break
-        if word != PAD:
-            answer += word + " "
-    return answer, finished
-
-
-# ---------------------------------------------------------------------------------------------
-max_len = 25
-embed_dim = 128
-ffn_dim = 128
-num_heads = 8
-num_layers = 2
-
-epochs = 1
-batch_size = 256
-dropout = 0.2
-learning_rate = 0.001
-xavier_initializer = True
-
-
-cha2idx, idx2cha = np.load(file="../data/tf_dict.npy", allow_pickle=True)
-train_input, train_output, train_target = np.load(file="../data/tf_train.npy", allow_pickle=True)
-valid_input, valid_output, valid_target = np.load(file="../data/tf_valid.npy", allow_pickle=True)
-vocab_size = len(cha2idx)
-print(vocab_size)
-for line in valid_output[:5]:
-    text = [idx2cha[x] + " " for x in line if idx2cha[x] != PAD]
-    print("".join(text))
-for line in valid_target[:5]:
-    text = [idx2cha[x] + " " for x in line if idx2cha[x] != PAD]
-    print("".join(text))
-
-
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Embedding, Dense, Dropout, LayerNormalization
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.utils import plot_model
-
 # embed_dim: word embedding size, encoder/decoder input/output size
 def encoders(inputs, embed_dim, num_heads, ffn_dim, num_layers):
     # inputs = Input(shape=(None, embed_dim))
@@ -172,7 +219,6 @@ def encoders(inputs, embed_dim, num_heads, ffn_dim, num_layers):
 
         outputs = Dense(ffn_dim, activation="relu")(attentions)
         outputs = Dense(embed_dim)(outputs)
-
         outputs = Dropout(rate=dropout)(outputs)
         outputs = LayerNormalization(epsilon=1e-6)(attentions + outputs)
 
@@ -196,112 +242,116 @@ def decoders(inputs, encoder_outputs, embed_dim, num_heads, ffn_dim, num_layers)
     outputs = embedding(inputs) + positional_encode
     outputs = Dropout(rate=dropout)(outputs)
 
-    outputs = inputs
     for i in range(num_layers):
         masked_attentions = multi_head_attention(
-            inputs, inputs, inputs, embed_dim, num_heads, mask=True
+            outputs, outputs, outputs, embed_dim, num_heads, mask=True
         )
         masked_attentions = Dropout(rate=dropout)(masked_attentions)
         masked_attentions = LayerNormalization(epsilon=1e-6)(masked_attentions + outputs)
 
-        self_attentions = multi_head_attention(
-            masked_attentions, encoder_outputs, encoder_outputs, embed_dim, num_heads, mask=True
+        attentions = multi_head_attention(
+            query=masked_attentions,
+            key=encoder_outputs,
+            value=encoder_outputs,
+            embed_dim=embed_dim,
+            num_heads=num_heads,
+            mask=False,
         )
-        self_attentions = Dropout(rate=dropout)(self_attentions)
-        self_attentions = LayerNormalization(epsilon=1e-6)(masked_attentions + self_attentions)
+        attentions = Dropout(rate=dropout)(attentions)
+        attentions = LayerNormalization(epsilon=1e-6)(masked_attentions + attentions)
 
-        outputs = Dense(ffn_dim, activation="relu")(self_attentions)
+        outputs = Dense(ffn_dim, activation="relu")(attentions)
         outputs = Dense(embed_dim)(outputs)
-
         outputs = Dropout(rate=dropout)(outputs)
-        outputs = LayerNormalization(epsilon=1e-6)(self_attentions + outputs)
+        outputs = LayerNormalization(epsilon=1e-6)(attentions + outputs)
 
     return outputs
 
 
-inputs = Input(shape=(None,))
-outputs = encoders(inputs, embed_dim, num_heads, ffn_dim, num_layers)
-encoder_model = Model(inputs=inputs, outputs=outputs)
-plot_model(model=encoder_model, to_file="images/13-encoder.png", show_shapes=True)
+def transformers(vocab_size, embed_dim, num_heads, ffn_dim, num_layers):
+    encoder_inputs = Input(shape=(None,))
+    encoder_outputs = encoders(encoder_inputs, embed_dim, num_heads, ffn_dim, num_layers)
+    encoder_model = Model(inputs=encoder_inputs, outputs=encoder_outputs)
+    plot_model(model=encoder_model, to_file="images/13-encoder.png", show_shapes=True)
+
+    decoder_inputs = Input(shape=(None,))
+    decoder_outputs = decoders(
+        decoder_inputs, encoder_outputs, embed_dim, num_heads, ffn_dim, num_layers
+    )
+    decoder_model = Model(inputs=[decoder_inputs, encoder_outputs], outputs=decoder_outputs)
+    plot_model(model=decoder_model, to_file="images/13-decoder.png", show_shapes=True)
+
+    enc_outputs = encoder_model(inputs=encoder_inputs)
+    dec_outputs = decoder_model(inputs=[decoder_inputs, enc_outputs])
+    outputs = Dense(units=vocab_size)(dec_outputs)
+
+    return Model(inputs=[encoder_inputs, decoder_inputs], outputs=outputs)
 
 
-# def make_model(features, labels, mode, params):
-#     TRAIN = mode == tf.estimator.ModeKeys.TRAIN
-#     EVAL = mode == tf.estimator.ModeKeys.EVAL
-#     PREDICT = mode == tf.estimator.ModeKeys.PREDICT
-#
-#     positional_encode = positional_encoding(params["max_len"], params["embed_dim"])
-#     if params["xavier_initializer"]:
-#         embedding_initializer = tf.keras.initializers.glorot_normal(seed=42)
-#     else:
-#         embedding_initializer = "uniform"
-#
-#     embedding = Embedding(
-#         input_dim=params["vocab_size"],
-#         output_dim=params["embed_dim"],
-#         embeddings_initializer=embedding_initializer,
-#     )
-#
-#     x_embedded_matrix = embedding(features["inputs"]) + positional_encode
-#     y_embedded_matrix = embedding(features["outputs"]) + positional_encode
-#
-#     encoder_outputs = encoders(
-#         x_embedded_matrix,
-#         params["embed_dim"],
-#         params["num_heads"],
-#         params["ffn_dim"],
-#         params["num_layers"],
-#     )
-#     decoder_outputs = decoders(
-#         y_embedded_matrix,
-#         encoder_outputs,
-#         params["embed_dim"],
-#         params["num_heads"],
-#         params["ffn_dim"],
-#         params["num_layers"],
-#     )
-#
-#     logits = Dense(params["vocab_size"])(decoder_outputs)
-#     predictions = tf.argmax(logits, axis=2)
-#
-#     if PREDICT:
-#         predictions = {"indexs": predictions, "logits": logits}
-#         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
-#
-#     labels_ = tf.one_hot(labels, depth=params["vocab_size"])
-#     loss = tf.reduce_mean(
-#         tf.compat.v1.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels_)
-#     )
-#     accuracy = tf.compat.v1.metrics.accuracy(labels=labels, predictions=predictions)
-#
-#     metrics = {"accuracy": accuracy}
-#     tf.summary.scalar("accuracy", accuracy[1])
-#
-#     if EVAL:
-#         return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
-#     assert TRAIN
-#
-#     optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=params["learning_rate"])
-#     train_op = optimizer.minimize(loss, global_step=tf.compat.v1.train.get_global_step())
-#
-#     return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+model = transformers(vocab_size, embed_dim, num_heads, ffn_dim, num_layers)
+optimizer = Adam(learning_rate=1e-4)
+model.compile(optimizer=optimizer, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+plot_model(model=model, to_file="images/13-transformer.png", show_shapes=True)
 
+callbacks = [
+    EarlyStopping(monitor="val_accuracy", min_delta=0.0001, patience=2),
+    ModelCheckpoint(
+        "../data/transformer_ex.keras",
+        monitor="val_accuracy",
+        verbose=1,
+        save_best_only=True,
+        save_weights_only=True,
+    ),
+]
 
-# transformer = tf.estimator.Estimator(
-#     model_fn=make_model,
-#     model_dir="../data/transformer",
-#     params={
-#         "vocab_size": vocab_size,
-#         "embed_dim": embed_dim,
-#         "num_heads": num_heads,
-#         "ffn_dim": ffn_dim,
-#         "num_layers": num_layers,
-#         "max_len": max_len,
-#         "learning_rate": learning_rate,
-#         "xavier_initializer": xavier_initializer,
-#     },
+# history = model.fit(
+#     x=[train_input, train_output],
+#     y=train_target,
+#     batch_size=batch_size,
+#     epochs=epochs,
+#     verbose=1,
+#     callbacks=callbacks,
+#     validation_split=0.2,
 # )
-#
-# transformer.train(input_fn=lambda: make_train_ds(), steps=epochs)
-# results = transformer.evaluate(input_fn=lambda: make_valid_ds())
+# results = model.evaluate(
+#     x=[valid_input, valid_output], y=valid_target, batch_size=batch_size, verbose=1
+# )
+# model.save_weights("../data/transformer_weight.h5")
 # print(results)
+
+
+def index2string(lines, dictionary):
+    sentence = []
+    finished = False
+    for line in lines:
+        sentence = [dictionary[idx] for idx in line]
+    answer = ""
+    for word in sentence:
+        if word == END:
+            finished = True
+            break
+        if word != PAD:
+            answer += word + " "
+    return answer, finished
+
+
+def chatbot(sentence):
+    inputs, _ = encoder_preprocessing([sentence], cha2idx)
+    outputs, _ = decoder_output_preprocessing([""], cha2idx)
+
+    answer = ""
+    for i in range(max_len):
+        if i > 0:
+            outputs, _ = decoder_output_preprocessing([answer], cha2idx)
+        predictions = model.predict(x=[inputs, outputs])
+        predictions = tf.argmax(predictions, axis=2).numpy()
+        answer, finished = index2string(predictions, idx2cha)
+        if finished:
+            break
+
+    return answer
+
+
+model.load_weights("../data/transformer_weight.h5")
+print(chatbot("안녕?"))
+print(chatbot("놀고 싶다."))
